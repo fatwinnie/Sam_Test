75ccfa7af5e7b0f731c3ef623305c8d131fa4678
fatwinnie <49060668+fatwinnie@users.noreply.github.com>
2020-09-02 18:41:15 +0800

    Update getBUG.py

--- HW4/getBUG.py
+++ HW4/getBUG.py
@@ -1,40 +1,77 @@
 import requests
+from bs4 import BeautifulSoup 
 import csv
-import codecs
-from bs4 import BeautifulSoup
-
-url = 'http://127.0.0.1:8000/bug.html'
-#url = 'http://127.0.0.1:8000/cgi-bin/message.py'
-resp = requests.get(url)
-resp.encoding='utf-8'
-soup = BeautifulSoup(resp.text, 'html.parser')
+import mysql.connector
 
 Title = []
 Content = []
 Time = []
-posts = soup.find_all("div", class_ = "container_history")
-#posts = soup.find_all(attrs={'class':'container_history'})
-
-for post in posts:
-    Title.append(post.find("div", class_ = "history_title").p.string)
-    Content.append(post.find("div", class_ = "history_content").p.string)
-    Time.append(post.find("div", class_ = "history_time").p.string)
-    #Title.append(post.p.string)
-#print(Title)
 
 
+def getData(url):
+    my_header={
+        'user-agent':'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0',
+        'cookie':'POOH_SID=af7ecddd305dbea92368cf7c14c86d0573fcee50'
+    }
+    resp = requests.get(url, headers= my_header)
+    resp.encoding ='utf-8'
+    #解析原始碼
+    soup = BeautifulSoup(resp.text, 'lxml')
+    find_title = soup.find_all("div",class_="history_title")
+    fine_content = soup.find_all("div",class_= "history_content")
+    find_time = soup.find_all("div",class_="history_time")
+   
+    for title in find_title:        
+        Title.append(title.p.string)
+    #print(Title)
+   
+
+    for content in fine_content:      
+        Content.append(content.p.string)
+    #print(Content)
+
+    for time in find_time:
+        Time.append(time.p.string)
+    #print(Time)
+   
+    
+def write_CSV():
+    with open('tingMsg.csv', 'w', newline='',encoding='utf_8') as csvfile:
+        writer = csv.writer(csvfile, Title)
+        writer.writerow(Title)
+        writer.writerow(Content)
+        writer.writerow(Time)
+
+def write_DB():
+    conn = mysql.connector.connect(      
+    host='localhost', # 主機名稱
+    database='test1', # 資料庫名稱
+    user='root',      # 帳號
+    password='2033',  # 密碼
+    charset='utf8') 
+
+    cursor = conn.cursor()
+    if Title != []:
+        for i in Title:
+            print(i)
+            sql = "INSERT INTO crawler (title) VALUES (%s)"
+            val = i
+            cursor.execute(sql, val)
+            onn.commit()
+            conn.close()
 
 
 
-# 開啟輸出的 CSV 檔案
-with open('tingMsg.csv', 'w', newline='',encoding='utf_8') as csvfile:
 
 
-    # 建立 CSV 檔寫入器
-    writer = csv.writer(csvfile, Title)
 
-    # 寫入一列資料
-    writer.writerow(Title)
-    writer.writerow(Content)
-    writer.writerow(Time)
+if __name__	== '__main__':
+    pageURL='http://127.0.0.1:8000/cgi-bin/message.py'
+    getData(pageURL)
+    write_CSV()
+    write_DB()
+  
+
+        
+
 
