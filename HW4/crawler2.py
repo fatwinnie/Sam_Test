af93ec983834b0fc29bc6d53f0d4c2df135c2629
fatwinnie <49060668+fatwinnie@users.noreply.github.com>
2020-09-04 11:57:20 +0800

    requests.Session()

---
+++ HW4/crawler2.py
@@ -0,0 +1,99 @@
+import requests
+from bs4 import BeautifulSoup 
+import csv
+import mysql.connector
+from mysql.connector import Error
+
+Title = []
+Content = []
+Time = []
+
+
+def getData(url):
+    my_header={
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
+        
+    }
+    my_data = {
+        'UserName': 'ting',
+        'pwd':'1234',
+
+    }
+    login_url = 'http://127.0.0.1:8000/cgi-bin/login.py'
+
+    session = requests.Session()
+    session.post(login_url, headers = my_header , data = my_data)
+    resp = session.get(url,headers= my_header)
+    #resp = requests.get(url, headers= my_header)
+    resp.encoding ='utf-8'
+    #解析原始碼
+    soup = BeautifulSoup(resp.text, 'lxml')
+    find_title = soup.find_all("div",class_="history_title")
+    fine_content = soup.find_all("div",class_= "history_content")
+    find_time = soup.find_all("div",class_="history_time")
+
+    print(type(resp.cookies),resp.cookies)
+    print(type(resp.headers),resp.headers)
+
+   
+    for title in find_title:        
+        Title.append(title.p.string)
+    #print(Title)
+   
+
+    for content in fine_content:      
+        Content.append(content.p.string)
+    #print(Content)
+
+    for time in find_time:
+        Time.append(time.p.string)
+    #print(Time)
+   
+    
+def write_CSV():
+    with open('tingMsg.csv', 'w', newline='',encoding='utf_8') as csvfile:
+        writer = csv.writer(csvfile, Title)
+        writer.writerow(Title)
+        writer.writerow(Content)
+        writer.writerow(Time)
+
+def write_DB():
+    conn = mysql.connector.connect(      
+    host='localhost', # 主機名稱
+    database='homework', # 資料庫名稱
+    user='root',      # 帳號
+    password='2033',  # 密碼
+    charset='utf8') 
+
+    cursor = conn.cursor()
+    # 取 Title[]長度來得到每個list的迴圈次數
+    a = len(Title)
+    for i in range(a):           
+        try:
+                     
+            sql = "INSERT INTO crawler (title, content,time) VALUES (%s, %s,%s);"
+            val = (Title[i],Content[i],Time[i])
+            #print(i)
+                    
+            cursor.execute(sql, val)
+            #print(sql)
+            
+                            
+        
+        except Error as e:
+            print("資料庫連接失敗：", e)
+
+           #conn.commit()
+            #conn.close()
+            
+
+    conn.commit()
+    conn.close()
+    # why conn.commit just can wirte here?
+ 
+
+if __name__	== '__main__':
+    pageURL='http://127.0.0.1:8000/cgi-bin/message.py'
+    getData(pageURL)
+    write_CSV()
+    write_DB()
