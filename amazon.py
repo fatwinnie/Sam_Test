3ba99bd3d21baeb74b94c703edcad1d480b99661
fatwinnie <49060668+fatwinnie@users.noreply.github.com>
2020-10-16 17:51:44 +0800

    Update amazon.py

--- amazon.py
+++ amazon.py
@@ -4,25 +4,25 @@ import csv
 import os,sys
 
 
-pageURL = 'https://www.amazon.com/'
-#search_URL = 'https://www.amazon.com/s?k=ultrawide+monitors&ref=nb_sb_noss_2'
-searchURL_template = 'https://www.amazon.com/s?k={}&ref=nb_sb_noss_2'
-search_text = 'ulttrawide monitor'
-search_term = search_text.replace(' ','+')
-#print(search_term)
-searchURL = searchURL_template.format(search_term) # put search_term into search_URL_template to replace {}
-#print(searchURL)
+def Search(type_text):
+    pageURL = 'https://www.amazon.com/'
+    #search_URL = 'https://www.amazon.com/s?k=ultrawide+monitors&ref=nb_sb_noss_2'
+    searchURL_template = 'https://www.amazon.com/s?k={}&ref=nb_sb_noss_2'
+    search_text = type_text
+    search_term = search_text.replace(' ','+')
+    #print(search_term)
+    searchURL = searchURL_template.format(search_term) # put search_term into search_URL_template to replace {}
+    #print(searchURL)
 
-driver = webdriver.Firefox()
-driver.get(searchURL)
-#print(driver.page_source)
-soup = BeautifulSoup(driver.page_source,'html.parser')
-result = soup.find_all('div',{'data-component-type': 's-search-result'})
-#driver.get(search_URL)
+    return searchURL
 
-records = []
-# list item 
-for item in result:    
+    
+    
+
+def Get_Record(item):
+    
+    # list item 
+       
     Item_Name = item.h2.a.text.strip()
     #print(Item_Name)
 
@@ -30,25 +30,49 @@ for item in result:
     #print(Item_URL)
     try:
         Price  = item.find('span','a-offscreen').text
-        print(Price)
+        #print(Price)
     except AttributeError:
-        print('no price')   
+        Price = ''    
     
-    Item_Star = item.find('i').text
-    #print(Item_Star)
+    try:
+        Item_Star = item.find('i').text
+        #print(Item_Star)
+    except AttributeError:
+        Item_Star = ''
 
     ReviewCount = item.find('span',{'class':'a-size-base','dir':'auto'}).text
     #print(ReviewCount)
-    result = (Item_Name, Price, Item_Star, ReviewCount, Item_URL)
-    print(type(result))
-    records.append(result)
- 
 
-# record as csv file
-with open('results.csv', 'w', newline='', encoding='utf-8') as f:
-    writer = csv.writer(f)
-    writer.writerow(['Description', 'Price', 'Rating', 'ReviewCount', 'Url'])
-    writer.writerows(records)
+    list_result = (Item_Name, Price, Item_Star, ReviewCount, Item_URL)
+    return list_result
+  
+
+def main(product_name):
+
+    searchURL = Search(product_name)
+
+    driver = webdriver.Firefox()
+    driver.get(searchURL)
+    #print(driver.page_source)
+    soup = BeautifulSoup(driver.page_source,'html.parser')
+    result = soup.find_all('div',{'data-component-type': 's-search-result'})
+
+    records = []
+    for item in result:
+        record = Get_Record(item)
+        records.append(record)
+    
+    # record as csv file
+    with open('results.csv', 'w', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(['Description', 'Price', 'Rating', 'ReviewCount', 'Url'])
+        writer.writerows(records)
+
+
+
+if __name__ == '__main__':
+
+    main('rtx 3080')
 
 
 
